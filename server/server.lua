30a9d5a4b706c9f4b8e085ee1b00d364d825d6ba
southieee <83940431+southieee@users.noreply.github.com>
2024-01-18 03:14:39 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,10 +1,10 @@
 
-RegisterNetEvent('nass_carplay:syncmusic')
-AddEventHandler('nass_carplay:syncmusic', function(peds, vehNet, data)
+RegisterNetEvent('carplay:syncmusic')
+AddEventHandler('carplay:syncmusic', function(peds, vehNet, data)
     local veh = NetworkGetEntityFromNetworkId(vehNet)
 	if veh ~= 0 then
         for k, v in pairs(peds) do
-            TriggerClientEvent("nass_carplay:playsound", v, data)
+            TriggerClientEvent("carplay:playsound", v, data)
         end
 	end
 end)
